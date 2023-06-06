463e5f0631f3e5629808aa15df70e82e76df9e89
Federico Quinteros <federiconquinteros@gmail.com>
2023-06-05 21:14:51 -0300

    primer subida

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Probando si el linkeo es correcto")
+
+var autor = "Jorge Luis Borges"
+autor = "Gabriel Garcia Marquez"
+
+console.log(autor)
+
+let precio = 4000
+console.log (precio)
+
+const IVA = 0.21
+console.log (IVA)
